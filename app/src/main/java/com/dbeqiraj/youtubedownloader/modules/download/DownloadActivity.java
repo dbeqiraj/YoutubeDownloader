35813ab5303cc5a2d4fb69ee7d5913a70891b9dc
Dionis Beqiraj <dbeqiraj.sohoal@gmail.com>
2018-04-15 11:41:25 +0200

    Bug fix

--- app/src/main/java/com/dbeqiraj/youtubedownloader/modules/download/DownloadActivity.java
+++ app/src/main/java/com/dbeqiraj/youtubedownloader/modules/download/DownloadActivity.java
@@ -103,6 +103,7 @@ public class DownloadActivity extends BaseActivity implements DownloadView {
     public void onVideoInfoDownloaded(Video video) {
         video.setVidTitle(video.getVidTitle()
                 .replaceAll("&quot;", "_")
+                .replaceAll("&#039;", "'")
                 .replaceAll("/", "_")
                 .replaceAll("\\\\", "_")
                 .replaceAll("&amp;", "&")
